b8c8e08884c40c975b0e348aef9fb1f9fdb44846
Garzzza7 <piotr.czapela@gmail.com>
2023-05-22 22:39:30 +0100

    asdasda

--- src/main/java/tqs/example/impostor/ImpostorApplication.java
+++ src/main/java/tqs/example/impostor/ImpostorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImpostorApplication {
-//asdasdasdasdadasdasdas
+//asdasdasdasdadasdasdasasdasd
 	public static void main(String[] args) {
 		SpringApplication.run(ImpostorApplication.class, args);
 	}
